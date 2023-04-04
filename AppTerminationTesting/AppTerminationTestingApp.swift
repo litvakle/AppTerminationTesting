d63b58354597687bc256c0f0646f22be705c1baa
litvakle <43027594+litvakle@users.noreply.github.com>
2023-04-04 12:24:08 +0500

    Initial Commit

---
+++ AppTerminationTesting/AppTerminationTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTerminationTestingApp.swift
+//  AppTerminationTesting
+//
+//  Created by Lev Litvak on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppTerminationTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
